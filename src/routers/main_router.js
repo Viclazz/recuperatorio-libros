9d52585c9f8b7bd8af3890f3b70b81e2491e9f17
ELENA_VICTORIA <eleviclazz@gmail.com>
2023-06-06 15:36:52 -0300

    tercer commit

--- src/routers/main_router.js
+++ src/routers/main_router.js
@@ -1,12 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mainController = require('../controllers/main-controller');
+var mainController = require("../controllers/main-controller");
 
-router.get('/buscarTodosLosLibros', mainController.libros);         
-router.post('/nuevoLibro', mainController.LibroNuevo)    
-router.get('/buscarLibro', mainController.filtroPorLibro) 
-router.get('/librosPorCategoria', mainController.Categorias)  
+router.get("/buscarTodosLosLibros", mainController.libros);
+router.post("/nuevoLibro", mainController.LibroNuevo);
+router.get("/buscarLibro", mainController.filtroPorLibro);
+router.get("/librosPorCategoria", mainController.Categorias);
 
-router.get('/libro/:id', mainController.libro) 
+router.get("/libro/:id", mainController.libro);
 
 module.exports = router;
