4769ae448381f845cca44389a3bf73893e2a99a5
ELENA_VICTORIA <eleviclazz@gmail.com>
2023-06-08 12:46:26 -0300

    retoques nav

--- reatcatalogo/src/components/NavBar.jsx
+++ reatcatalogo/src/components/NavBar.jsx
@@ -4,26 +4,26 @@ function NavBar() {
   return (
     <>
       <div className="navbar">
-        <ul>
+       
           <Link to={`/libros`} className="button">
             Libros
           </Link>
-        </ul>
-        <ul>
+        
+        
           <Link to={`/categoria`} className="button">
             Categorias
           </Link>
-        </ul>
-		<ul>
-        <Link to={`/nuevoLibro`} className="button">
-          Nuevo Libro
-        </Link>
-		</ul>
-    <ul>
-        <Link to={`/nuevoAutor`} className="button">
-          Nuevo Autor
-        </Link>
-		</ul>
+       
+        
+          <Link to={`/nuevoLibro`} className="button">
+            Nuevo Libro
+          </Link>
+        
+        
+          <Link to={`/nuevoAutor`} className="button">
+            Nuevo Autor
+          </Link>
+        
       </div>
     </>
   );
